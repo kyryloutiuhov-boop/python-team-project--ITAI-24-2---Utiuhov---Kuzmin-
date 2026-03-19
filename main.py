a4e40b2d9ed2de1e4872385a869ac8eaacffcbe1
kyryloutiuhov-boop <kyrylo.utiuhov@nure.ua>
2026-03-19 23:52:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from password_utils import generate_password, check_strength
 def main():
     print("=== Password Generator ===")
 
-    length = int(input("Enter password length: "))
+    length = int(input("Введіть довжину пароля: "))
 
-    use_digits = input("Include digits? (y/n): ") == "y"
-    use_symbols = input("Include symbols? (y/n): ") == "y"
+    use_digits = input("Буде мати цифри? (так/ні): ") == "так"
+    use_symbols = input("Буде мати спеціальні символи? (так/ні): ") == "так"
 
     password = generate_password(length, use_digits, use_symbols)
 
     strength = check_strength(password)
 
-    print("\nGenerated password:", password)
-    print("Strength:", strength)
+    print("\nЗгенерований пароль:", password)
+    print("Довжина:", strength)
 
 
 if __name__ == "__main__":
